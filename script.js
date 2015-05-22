6d49ebb86c3971681e54cc952afcf43d78406e17
thleap <zozo4869@gmail.com>
2015-05-22 12:42:40 +0900

    Add trigger machine gun and Tweak style

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 $(function() {
   var trigger = $('.js-trigger');
   var triggerTarget = $('.js-trigger-target');
+  var triggerMachineGun = $('.js-trigger-machine-gun');
 
-  trigger.find(triggerTarget).addClass("is-noise");
-  trigger.on("click", function() {
-    $(this).find(triggerTarget).removeClass("is-noise");
+  trigger.find(triggerTarget).addClass('is-noise');
+  trigger.on('click', function() {
+    $(this).find(triggerTarget).removeClass('is-noise');
+    $(this).css('cursor', 'inherit');
+  });
+  triggerMachineGun.on('click', function() {
+    var openWord = 'すべて開く';
+    var closeWord = 'すべて隠す';
+
+    triggerTarget.toggleClass('is-noise');
+    if ($(this).text() == closeWord) {
+      $(this).text(openWord);
+    } else {
+      $(this).text(closeWord);
+    };
+    trigger.css('cursor', 'inherit');
   });
 });
